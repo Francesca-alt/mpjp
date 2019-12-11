4d89ccf63801bffd07da9586d836d9772972510b
Francesca Iacone <iacone_francy@hotmail.it>
2019-12-11 10:19:04 +0100

    exercise3 correct

--- src/main/java/s044/Exercise3.java
+++ src/main/java/s044/Exercise3.java
@@ -3,17 +3,20 @@ package s044;
 public class Exercise3 {
 	public static void main(String[] args) {
 		int[] array = { 7, 3, 1, 2, 4, 8 };
-		int even = array[0];
 		int counter = 0;
-	
+		var sum = 0;
 
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] % 2 == 0) {
 				counter = counter + 1;
 
 			} else {
-				continue; }
+				sum = sum + array[i];
 			}
-		System.out.println(" your array has" + counter + "even numbers");
+
+			System.out.println(counter);
+			System.out.println(sum);
+
 		}
+	}
 }
