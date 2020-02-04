ea43f74082ad41581690f84734f68106854a41ae
Francesca Iacone <iacone_francy@hotmail.it>
2020-02-04 17:32:48 +0100

    polimorfismo

--- src/main/java/s070ex/Bus.java
+++ src/main/java/s070ex/Bus.java
@@ -4,5 +4,5 @@ public class Bus extends Vehicle implements Conditioning {
     @Override
     public void setTemperature(int newTemp) {
         System.out.println("Bus temperature now is " + newTemp);
-    }
+    } 
 }
